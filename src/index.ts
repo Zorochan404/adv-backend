a756b7db03026cc09cd36087b3ad881e52e25e62
Tirthankar03 <tirthankar.nath2@gmail.com>
2025-08-10 12:10:13 +0530

    rate limit update to 1 sec

--- src/index.ts
+++ src/index.ts
@@ -59,7 +59,8 @@ app.use(
 
 // Rate limiting
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 1000, // 1 sec
+  //15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
   message: {
     success: false,
